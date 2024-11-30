7f237780fc98c70bfb6880e96c91a67cdb2297ba
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2024-11-30 01:42:51 +0100

    moving zig files and change ci file

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = program_name,
-        .root_source_file = b.path("src/zig/main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/zig/parser.zig"),
+        .root_source_file = b.path("src/parser.zig"),
         .target = target,
         .optimize = optimize,
     });
