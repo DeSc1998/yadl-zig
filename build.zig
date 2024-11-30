3a2aa53867d302be68901ee02f82af618904e55b
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2024-11-30 03:28:45 +0100

    renaming for zig naming convention (zig test)

--- build.zig
+++ build.zig
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/parser.zig"),
+        .root_source_file = b.path("src/Parser.zig"),
         .target = target,
         .optimize = optimize,
     });
