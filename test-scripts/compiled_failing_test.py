77f80017cb160b6fdaf4df7e277ac34388ef3d94
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2025-05-02 15:26:40 +0200

    added test cases for compiled code

---
+++ test-scripts/compiled_failing_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+from helper import load_configs, run_failing_test
+
+(configurations, file_names) = load_configs('test/failing', is_compiled=True)
+
+
+@pytest.mark.parametrize("config", configurations, ids=file_names)
+def test_config(config):
+    run_failing_test(config)
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if item.get_marker('timeout') is None:
+            item.add_marker(pytest.mark.timeout(10))
