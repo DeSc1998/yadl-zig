f74f5aa81612f7cbfe68128f1bfa9bcf36314e55
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2025-08-23 16:26:59 +0200

    added maschine specific function signatures and remove optional arguments

--- lib/stdlib/type.zig
+++ lib/stdlib/type.zig
@@ -1,15 +1,16 @@
 const std = @import("std");
 
 const interpreter = @import("../interpreter.zig");
+const maschine = @import("../maschine.zig");
 const expression = @import("../expression.zig");
 const Scope = @import("../Scope.zig");
 
 pub const Error = std.mem.Allocator.Error || interpreter.Error;
+pub const MaschineError = maschine.Error;
 
 pub const Arity = struct {
     // NOTE: unnamed from the perspective of the call site
     unnamed_count: u32,
-    optionals: [][]const u8 = &[0][]const u8{},
     has_variadics: bool = false,
 };
 
@@ -20,26 +21,16 @@ pub const OptionalArg = struct {
 
 pub const CallMatch = struct {
     unnamed_args: []expression.Value,
-    optional_args: []OptionalArg,
     var_args: ?[]expression.Value,
 
     pub fn init(
         unnamed_args: []expression.Value,
-        optionals: ?[]OptionalArg,
         var_args: ?[]expression.Value,
     ) CallMatch {
         return .{
             .unnamed_args = unnamed_args,
-            .optional_args = if (optionals) |ops| ops else (([0]OptionalArg{})[0..]),
             .var_args = var_args,
         };
-    }
-
-    pub fn optional_by_name(self: CallMatch, name: []const u8) ?expression.Value {
-        for (self.optional_args) |arg| {
-            if (std.mem.eql(u8, arg.name, name)) return arg.expr;
-        }
-        return null;
     }
 };
 
@@ -47,3 +38,6 @@ pub const StdlibFn = *const fn (CallMatch, *Scope) Error!void;
 pub const NextFn = *const fn ([]expression.Value, *Scope) Error!void;
 pub const HasNextFn = *const fn ([]expression.Value, *Scope) Error!void;
 pub const PeekFn = *const fn ([]expression.Value, *Scope) Error!void;
+
+pub const IntrinsicFn = *const fn (*maschine.Maschine) MaschineError!void;
+pub const IntrIterFn = *const fn ([]expression.Value, *maschine.Maschine) MaschineError!void;
